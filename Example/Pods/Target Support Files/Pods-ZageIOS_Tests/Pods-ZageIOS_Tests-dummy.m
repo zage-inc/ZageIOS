ed76d17bf936884a6cacddcd73d48344e1641c60
Michael Sun <michael@zage.app>
2021-10-28 13:31:06 -0700

    Pushing initial Cocoapod template

---
+++ Example/Pods/Target Support Files/Pods-ZageIOS_Tests/Pods-ZageIOS_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZageIOS_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZageIOS_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZageIOS/ZageIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZageIOS : NSObject
+@end
+@implementation PodsDummy_ZageIOS
+@end
